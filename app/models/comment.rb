13af55787130c62f58a255a7e117425c47c19e81
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-14 21:59:27 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: :all_blank
   validates :content, presence: true
+  validates :user, presence: true
 end
