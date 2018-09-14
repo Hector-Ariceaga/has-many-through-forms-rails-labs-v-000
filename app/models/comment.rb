e35e3b6ca122210121a42be089dd1b82833f844a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-14 21:04:56 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,15 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def comments_attributes=(comments_attributes)
+    comments_attributes.values.each do |comment_attribute|
+      if comment_attribute[:content].present?
+        comment = comement.find_or_create_by(comment_attribute)
+
+        if !self.comments.include?(comment)
+          self.post_comments.build(comment: comment)
+        end
+      end
+    end
+  end
 end
